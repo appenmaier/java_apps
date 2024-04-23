cde2dc6bdece582da13f579ed55954916740c022
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-04-23 08:43:04 +0200

    Update LED App

--- led/App.java
+++ led/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
     Scene scene = new Scene(root);
-    primaryStage.setTitle("Leuchte");
+    primaryStage.setTitle("LED");
     primaryStage.setScene(scene);
     primaryStage.setResizable(false);
     primaryStage.show();
