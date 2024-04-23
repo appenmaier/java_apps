6612e5bd68b2b84f8576be04d9f76b50ca0432aa
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-04-23 08:49:46 +0200

    Update Balls App

--- balls/Controller.java
+++ balls/Controller.java
@@ -4,10 +4,12 @@ import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class Controller implements Initializable {
@@ -20,11 +22,33 @@ public class Controller implements Initializable {
   public void initialize(URL location, ResourceBundle resources) {
     model = Model.getInstance();
 
-    canvas.setOnMouseClicked(e -> {
+    canvas.setOnMouseClicked(new CanvasClickedEventHandler());
+
+    BallsAnimationTimer timer = new BallsAnimationTimer();
+    timer.start();
+  }
+
+  private class BallsAnimationTimer extends AnimationTimer {
+    @Override
+    public void handle(long l) {
+      GraphicsContext g = canvas.getGraphicsContext2D();
+      g.setFill(Color.WHITE);
+      g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+      for (Ball ball : model.getBalls()) {
+        ball.move(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        g.setFill(ball.getColor());
+        g.fillOval(ball.getX(), ball.getY(), ball.getR() * 2, ball.getR() * 2);
+      }
+    }
+  }
+
+  private class CanvasClickedEventHandler implements EventHandler<MouseEvent> {
+    @Override
+    public void handle(MouseEvent event) {
       Random random = new Random();
       int r = random.nextInt(25, 50);
-      int x = (int) e.getX();
-      int y = (int) e.getY();
+      int x = (int) event.getX();
+      int y = (int) event.getY();
       double red = random.nextDouble(1);
       double green = random.nextDouble(1);
       double blue = random.nextDouble(1);
@@ -33,22 +57,7 @@ public class Controller implements Initializable {
       int speedY = random.nextInt(5, 15);
       Color color = new Color(red, green, blue, opacity);
       model.addBall(new Ball(r, x, y, color, speedX, speedY));
-    });
-
-    AnimationTimer timer = new AnimationTimer() {
-      @Override
-      public void handle(long l) {
-        GraphicsContext g = canvas.getGraphicsContext2D();
-        g.setFill(Color.WHITE);
-        g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        for (Ball ball : model.getBalls()) {
-          ball.move(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-          g.setFill(ball.getColor());
-          g.fillOval(ball.getX(), ball.getY(), ball.getR() * 2, ball.getR() * 2);
-        }
-      }
-    };
-    timer.start();
+    }
   }
 
 }
