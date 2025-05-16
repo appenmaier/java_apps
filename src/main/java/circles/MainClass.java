72d77197e2f20db8e767de1596c94f93c8d9231b
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2025-05-16 15:11:12 +0200

    Add args

--- src/main/java/circles/MainClass.java
+++ src/main/java/circles/MainClass.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class MainClass {
 
    public static void main(String[] args) {
-      Application.launch(App.class);
+      Application.launch(App.class, args);
    }
 
 }
